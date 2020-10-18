9b0af206e78b4cf0e4c763888ce6ccfa64c38af0
2133069 <37394551+2133069@users.noreply.github.com>
2020-10-18 20:10:06 +0800

    Update Sample.java
    Added a new print line

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
 public class  Sample{
     public static void main (String[] args ){
         System.out.println("This is a sample");
+        
+        System.out.println("This is a newly added code");
     }
 }
